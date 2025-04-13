41b21c736c54bc559b67f389cf4531fdc5536467
himakarborra9 <himakarborra9@gmail.com>
2025-04-13 22:25:42 +0530

    modifications on day4

--- Day4/File_Main.py
+++ Day4/File_Main.py
@@ -1,5 +1,6 @@
 from pkg.file import File
+import datetime
 
-fs = File(r"C:\Users\Himakar\Desktop\handson\Day3")
-print(fs.getMaxSizeFile(5))
-fs.getLatestFiles()
+fs = File(r"C:\Users\Himakar\Desktop\handson")
+# print(fs.getMaxSizeFile(5))
+print(fs.getLatestFiles(datetime.date(2025,4,7)))
--- Day4/pkg/file.py
+++ Day4/pkg/file.py
@@ -1,6 +1,6 @@
 import os
 import glob
-
+import datetime
 
 class File:
     def __init__(self,path):
@@ -8,13 +8,29 @@ class File:
         
     def getMaxSizeFile(self,n):
         size = []
-        for _,_,dirfn in os.walk(self.path):
+        for root,_,dirfn in os.walk(self.path):
             for f in dirfn:
-                size.append((f,os.path.getsize(os.path.join(self.path,f))))
+                c_path = os.path.join(root,f)
+                if os.path.isfile(c_path):
+                    size.append((c_path,os.path.getsize(c_path)))
         l = sorted(size, key=lambda f : f[1],reverse = True)
         return l[:n]
         
-    def getLatestFiles(self):
+    def getLatestFiles(self,date):
         files = []
-        for f in os.path.join(self.path,"*"):
-            
+        
+        def check_folder(path):
+            for f in glob.glob(os.path.join(path,"*")):
+                if os.path.isfile(f):
+                    l_time = os.path.getmtime(f)
+                    f_date = datetime.date.fromtimestamp(l_time)
+                        
+                    if f_date > date :
+                        files.append(f)
+                else:
+                    check_folder(f)
+                    
+        check_folder(self.path)
+        
+        return files 
+        
